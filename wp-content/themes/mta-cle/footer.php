8890c014dbe86b14c37c3fec32cd2f838b2ba18c
serumax <serumax@192.168.1.102>
2013-04-11 14:00:31 -0400

    style

--- wp-content/themes/mta-cle/footer.php
+++ wp-content/themes/mta-cle/footer.php
@@ -1,46 +1,26 @@
-<?php
+            </div><!--contenido-->
+            <footer class="footerBox clearfix">
+                <div class="left logo">
+                    <img src="ruta al logo" alt="Logotipo de CLE">
+                </div>
+                <div class="left">
+                    <nav id="menuFooter">
+                        <?php
+                            if (is_user_logged_in()) {
 
-/**
+                                wp_nav_menu(array('theme_location' => 'primary'));
+                            } else {
 
- * The template for displaying the footer.
-
- *
-
- * Contains footer content and the closing of the
-
- * #main and #page div elements.
-
- *
-
- * @package WordPress
-
- * @subpackage Twenty_Twelve
-
- * @since Twenty Twelve 1.0
-
- */
-
-?>
-
-</div><!--contenido-->
-
-<div class="footerBox" align="center">
-
-Todos los derechos reservados - C Level Executive Club - Para mayor información contactarse a <a href="mailto:contacto@cleclub.cl">contacto@cleclub.cl</a><br /> <span style="font-size:90%;margin-top:7px;color:#999;"> Desarrollado por <a href="http://www.mta.cl">media technology & aRT</a></span>
-
-</div>
-
-</div><!--/wrapper-->
-
-</div><!--/container-->
-
-
-
-
-
-<?php wp_footer(); ?>
-
-</body>
-
+                                wp_nav_menu(array('theme_location' => 'public'));
+                            };
+                        ?>
+                    </nav>
+                    <a href="mailto: <?php echo get_bloginfo( 'admin_email');?>" title="E-mail de contacto"><?php echo get_bloginfo( 'admin_email');?></a>
+                </div>
+            </footer>
+        </div><!--/wrapper-->
+    </div><!--/container-->
+    <?php wp_footer(); ?>
+    </body>
 </html>
 
