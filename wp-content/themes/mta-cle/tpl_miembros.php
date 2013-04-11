3bee06f2574da15dca2899bac66180fb2de81f27
Francisco <francisco@ida.cl>
2013-04-11 13:36:48 -0400

    Barra superior noticias diario financiero

--- wp-content/themes/mta-cle/tpl_miembros.php
+++ wp-content/themes/mta-cle/tpl_miembros.php
@@ -23,18 +23,17 @@ the_post();
         $i = 1;
         $blogusers = get_users('orderby=nicename&role=subscriber');
         foreach ($blogusers as $user) {
-            $last = !$i % 4 ? ' last' : '';
-            echo '<div class="one_fourth'. $last.'">
+            $last = $i % 3 == false ? ' last' : '';
+            echo '<div class="one_third'.$last.'">
                     <div class="bio">
                         <div id="fotoMiembro">
-                            <a href="'. get_author_posts_url($user->ID).'" title="'.$user->display_name.'">'.  get_wp_user_avatar($user->ID,"medium").'</a>
+                            <a href="'. get_author_posts_url($user->ID).'" title="'.$user->display_name.'">'.  get_wp_user_avatar($user->ID,"portadaPerfil").'</a>
                         </div>
                         <div id="infoMiembro">
                             <hgroup class="head">
                                 <h4>'.$user->display_name.'</h4>
                                 <h5>'.the_field('cargo').'</h5>
                             </hgroup>
-                            <span class="header_shadow"></span>
                         </div>
                     </div>
                 </div>';
