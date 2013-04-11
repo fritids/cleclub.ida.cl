f18c0b5841e605c7186a558ad3da9bd0c37b8bde
serumax <serumax@192.168.1.102>
2013-04-11 12:46:15 -0400

    style

--- wp-content/themes/mta-cle/author.php
+++ wp-content/themes/mta-cle/author.php
@@ -1,38 +1,23 @@
 <?php get_header(); ?>
-
 <div id="contenido">
-
-
-
-
-
-
     <div id="leftSide">
-
-
         <?php
         if (is_user_logged_in()) {
-
             the_post();
-            ?>
-
+            global $wp_query;
+            $curauth = $wp_query->get_queried_object();            
+        ?>
             <div id="fotoMiembroBig" class="bioBig">
-                <?php echo get_wp_user_avatar($user->ID,"medium"); ?> 
+                <?php echo get_wp_user_avatar($curauth->ID, "medium"); ?> 
             </div>				
-
             <div id="bioMiembro">
-                <h1><?php the_title(); ?></h1>
-                <p class="subTitulo"><?php the_field('cargo'); ?></p>
-                <p class="subTitulo"><?php the_field('empresa'); ?></p	>                
-                <p class="subTitulo"><?php the_field('telefono'); ?></p>                
-                <p class="subTitulo ultimo"><a href="<?php the_field('mail'); ?>"><?php the_field('mail'); ?></a></p>                                
-
-                <div id="bioText">
-                    <?php the_content(); ?>
-                </div>
+                <h1><?php echo $curauth->display_name; ?></h1>
+                <p class="subTitulo"><?php the_field('cargo',"user_$curauth->ID"); ?></p>
+                <p class="subTitulo"><?php the_field('empresa',"user_$curauth->ID"); ?></p	>                
+                <p class="subTitulo"><?php the_field('telefono',"user_$curauth->ID"); ?></p>                
+                <p class="subTitulo"><a href="mailto:<?php the_field('mail',"user_$curauth->ID");?>"><?php the_field('mail',"user_$curauth->ID"); ?></a></p>                                
                 <p class="subTitulo primero"><a href="/miembros-del-club/"> <img src="<?php echo get_template_directory_uri(); ?>/_img/volver.gif" style="margin-bottom:-3px;margin-right:10px;" /> volver al listado de miembros</a></p>
             </div>
-
             <?
         } else {
 
@@ -40,12 +25,8 @@
         };
         ?>       
 
-    </div><!--/leftSide -->
-
+    </div>
     <?php get_sidebar(); ?>
-
-
 </div><!--pageContent-->
-
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
