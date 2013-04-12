39996a0dfb41964ac31750a443c841dca2d6fa00
Francisco <francisco@ida.cl>
2013-04-12 17:27:45 -0400

    sss

--- wp-content/themes/mta-cle/searchform.php
+++ wp-content/themes/mta-cle/searchform.php
@@ -1,10 +1,9 @@
 	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
             <span style="margin-right:20px; color:#333;">
-                <a href="/contacto/">
+                <a class="contactoButton" href="/contacto/">
                     Contacto 
-                    <img src="<?php bloginfo('template_directory');?>/_img/contacto.gif" style="margin-bottom:-3px;margin-right:10px;display:inline;"> 
-                </a>|
+                </a>
             </span>
-                <input type="text" class="field searchInput round5" name="s" id="s" placeholder="Buscar" />
+                <input type="text" class="field searchInput" name="s" id="s" placeholder="Buscar" />
 		<input type="image" style="margin-bottom:-3px" value="buscar" id="searchsubmit" name="submit" class="submit" src="<?php bloginfo('template_directory');?>/_img/lupa.gif">
 	</form>
