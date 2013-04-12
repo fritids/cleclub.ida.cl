c5aa8551baff8afc700a5c85da2ab5107933ad7d
Francisco <francisco@ida.cl>
2013-04-12 10:17:57 -0400

    Barra superior noticias diario financiero

--- wp-content/themes/mta-cle/searchform.php
+++ wp-content/themes/mta-cle/searchform.php
@@ -1,5 +1,10 @@
 	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<span style="margin-right:20px; color:#333;"><a href="/contacto/">Contacto <img src="<?php bloginfo('template_directory');?>/_img/contacto.gif" style="margin-bottom:-3px;margin-right:10px"> </a>|</span>
-        <input type="text" class="field searchInput round5" name="s" id="s" placeholder="Buscar" />
+            <span style="margin-right:20px; color:#333;">
+                <a href="/contacto/">
+                    Contacto 
+                    <img src="<?php bloginfo('template_directory');?>/_img/contacto.gif" style="margin-bottom:-3px;margin-right:10px;display:inline;"> 
+                </a>|
+            </span>
+                <input type="text" class="field searchInput round5" name="s" id="s" placeholder="Buscar" />
 		<input type="image" style="margin-bottom:-3px" value="buscar" id="searchsubmit" name="submit" class="submit" src="<?php bloginfo('template_directory');?>/_img/lupa.gif">
 	</form>
