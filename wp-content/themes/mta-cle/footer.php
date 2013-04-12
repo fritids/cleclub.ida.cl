8e8d7573ce71312de2eca257780e50303e705c87
Francisco <francisco@ida.cl>
2013-04-12 09:16:05 -0400

    Barra superior noticias diario financiero

--- wp-content/themes/mta-cle/footer.php
+++ wp-content/themes/mta-cle/footer.php
@@ -1,25 +1,41 @@
             </div><!--contenido-->
-            <footer class="footerBox clearfix">
-                <div class="left logo">
-                    <img src="ruta al logo" alt="Logotipo de CLE">
-                </div>
-                <div class="left">
-                    <nav id="menuFooter">
-                        <?php
-                            if (is_user_logged_in()) {
-
-                                wp_nav_menu(array('theme_location' => 'primary'));
-                            } else {
-
-                                wp_nav_menu(array('theme_location' => 'public'));
-                            };
-                        ?>
-                    </nav>
-                    <a href="mailto: <?php echo get_bloginfo( 'admin_email');?>" title="E-mail de contacto"><?php echo get_bloginfo( 'admin_email');?></a>
-                </div>
-            </footer>
         </div><!--/wrapper-->
     </div><!--/container-->
+    <footer class="footerBox clearfix">
+             <div class="center">
+                 <div class="left logo">
+                     <img src="<?php bloginfo('template_directory'); ?>/_img/logoFooter.png" alt="Logotipo de CLE">
+                 </div>
+                 <div class="menuBlock">
+                     <nav id="menuFooter">
+                         <?php
+                         if (is_user_logged_in()) {
+
+                             wp_nav_menu(array('theme_location' => 'primary','items_wrap'=> '<ul class="menuFoot">%3$s</ul>'));
+                         } else {
+
+                             wp_nav_menu(array('theme_location' => 'public','items_wrap'=> '<ul class="menuFoot">%3$s</ul>'));
+                         };
+                         ?>
+                     </nav>
+                     <ul class="members">
+                         <li>
+                             <a href="http://www.cdduc.cl/" title="Centro de Desarrollo Directivo UC" rel="#"><img src="<?php bloginfo('template_directory'); ?>/_img/logoUC.png"/></a>
+                         </li>
+                         <li>
+                             <a href="http://www.df.cl/" title="Diario Financiero" rel="#"><img src="<?php bloginfo('template_directory'); ?>/_img/logoDF.png"/></a>
+                         </li>
+                         <li>
+                             <a href="http://www.eychile.cl/" title="Ernst & Young" rel="#"><img src="<?php bloginfo('template_directory'); ?>/_img/logoEY.png"/></a>
+                         </li>
+                     </ul>
+                  <a class="contactMail clearfix" href="mailto: <?php echo get_bloginfo('admin_email'); ?>" title="E-mail de contacto"><?php echo get_bloginfo('admin_email'); ?></a>   
+                 </div>
+                
+                 
+             </div> 
+             
+            </footer>
     <?php wp_footer(); ?>
     </body>
 </html>
