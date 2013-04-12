4181d7b06cfd177da7686a15e662620bbb939acf
serumax <serumax@192.168.1.102>
2013-04-12 09:38:56 -0400

    style

---
+++ wp-content/themes/mta-cle/tpl_representantes.php
@@ -0,0 +1,35 @@
+<?php
+/**
+  Template Name: representantes
+ */
+?>
+<?php
+get_header();
+the_post();
+?>
+
+<div id="contenido">
+    <div id="pageContent" class="clearfix">
+        <h1><?php the_title(); ?></h1>
+        <?php
+        $i = 1;
+        $blogusers = get_users('orderby=nicename&role=representante');
+        foreach ($blogusers as $user) {
+            $last = $i % 3 == false ? ' last' : '';
+            echo '<div class="one_third'.$last.'">
+                    <div class="bio">
+                        <div class="fotoMiembro">
+                            <a href="'. get_author_posts_url($user->ID).'" title="'.$user->display_name.'">'.  get_wp_user_avatar($user->ID,"portadaPerfil").'</a>
+                        </div>
+                        <div class="infoMiembro">
+                                <h4><a href="'. get_author_posts_url($user->ID).'" title="'.$user->display_name.'">'.$user->display_name.'</a></h4>
+                                <p>'.get_field('cargo', "user_$user->ID").'</p>                            
+                                <p>'.get_field('empresa', "user_$user->ID").'</p>                            
+                        </div>
+                    </div>
+                </div>';
+            $i++;
+        }
+        ?>
+    </div>
+    <?php get_footer(); ?>
