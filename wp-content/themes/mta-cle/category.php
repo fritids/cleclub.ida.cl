3e55602b24313a1ed6336b966c006387d4b2898f
Francisco <francisco@ida.cl>
2013-04-16 13:19:57 -0400

    ss

--- wp-content/themes/mta-cle/category.php
+++ wp-content/themes/mta-cle/category.php
@@ -17,6 +17,7 @@
 get_header();
 ?>
 <div id="contenido">
+<<<<<<< HEAD
     <div id="pageContent">
         <h1><?php echo single_cat_title('', false) ?></h1>
         <div id="wNoticias">
@@ -42,6 +43,30 @@ get_header();
                     <?php echo cortar($post->post_content, 150); ?>
                         </div>
                     </li>
+=======
+<div id="pageContent">
+<h1><?php echo single_cat_title( '', false ) ?></h1>
+<div id="wNoticias">
+    <ul>
+        
+        <?php while(have_posts()) : the_post(); ?> 
+            <li>
+                <div id="picNoticia">
+                     <?php the_post_thumbnail('notas'); ?>
+                </div>
+                <?php
+                    $fechaEvento = get_field('fecha');
+                    $dia = date('d', strtotime($fechaEvento));
+                    $mes = traduceMes(date('m', strtotime($fechaEvento)));
+                    $ano = date('Y', strtotime($fechaEvento));
+                 ?>
+                <div id="bajadaNoticia">
+                    <span class="dateEvent"><?php echo $dia . ' de ' . $mes . ' de ' . $ano ?> , <?php the_field('hora'); ?> hrs., <?php the_field('lugar'); ?></span>
+                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                            <?php echo cortar($post->post_content, 150); ?>
+                </div>
+            </li>
+>>>>>>> ss
 <?php endwhile; ?>
             </ul>
         </div><!--/wNoticias-->
