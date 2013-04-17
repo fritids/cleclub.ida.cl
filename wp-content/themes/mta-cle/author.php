a56d5630965ab6e10f593b1a1d303975bc6b0a24
Francisco <francisco@ida.cl>
2013-04-17 11:25:52 -0400

    Barra superior noticias diario financiero

--- wp-content/themes/mta-cle/author.php
+++ wp-content/themes/mta-cle/author.php
@@ -15,7 +15,7 @@
                 
                 <p class="subTitulo"><strong>Empresa:</strong> <?php the_field('empresa',"user_$curauth->ID"); ?></p>
                 <p class="subTitulo"><strong>Cargo:</strong> <?php the_field('cargo',"user_$curauth->ID"); ?></p>
-                <p class="subTitulo"><strong>Telefono:</strong> <?php the_field('telefono',"user_$curauth->ID"); ?></p>                
+                <p class="subTitulo"><strong>Teléfono:</strong> <?php the_field('telefono',"user_$curauth->ID"); ?></p>                
                 <p class="subTitulo"><strong>Mail: </strong> <a href="mailto:<?php the_field('mail',"user_$curauth->ID");?>"><?php the_field('mail',"user_$curauth->ID"); ?></a></p> 
                 <p class="subTitulo"><strong>Edad: </strong><?php the_field('edad',"user_$curauth->ID"); ?></a></p>
                 <p class="subTitulo"><strong>Estado Civil: </strong><?php the_field('estado_civil',"user_$curauth->ID"); ?></a></p>
