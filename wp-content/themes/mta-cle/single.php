1cc4c7c42ae4d079b21cc097edfa91c28b95bb5e
Francisco <francisco@ida.cl>
2013-05-24 15:47:19 -0400

    asdasd

--- wp-content/themes/mta-cle/single.php
+++ wp-content/themes/mta-cle/single.php
@@ -62,8 +62,10 @@
                      echo '<div class="exclusive"><strong>Descarga exclusiva para miembros de CLE CLUB</strong><br /><small><a href="/solicitud-de-membresia/">Solicita Membresía</a> o Inicia Sesion</small>';
                 };
                 ?>
+                <div class="social">
                     <a class="lkd"target="_blank" href="http://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink();?>&title=<?php the_title();?>&summary=<?php the_title();?>&source={articleSource}">linkedin</a>
                     <a class="twitter" href="" rel="" alt="">twitter</a>
+                </div>
                 <?php
                 
             endwhile;
