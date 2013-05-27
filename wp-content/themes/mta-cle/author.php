a3e68067a48d9eab09b876a5faaa2ef616a45516
Francisco <francisco@ida.cl>
2013-05-27 18:51:10 -0400

    asdsa

--- wp-content/themes/mta-cle/author.php
+++ wp-content/themes/mta-cle/author.php
@@ -15,15 +15,24 @@
             <div id="bioMiembro">
                 <h1><?php echo $curauth->display_name; ?></h1>
                 
-                <p class="subTitulo"><strong>Empresa:</strong> <?php the_field('empresa',"user_$curauth->ID"); ?></p>
-                <p class="subTitulo"><strong>Cargo:</strong> <?php the_field('cargo',"user_$curauth->ID"); ?></p>
-                <p class="subTitulo"><strong>Teléfono:</strong> <?php the_field('telefono',"user_$curauth->ID"); ?></p>                
-                <p class="subTitulo"><strong>Mail: </strong> <a href="mailto:<?php the_field('mail',"user_$curauth->ID");?>"><?php the_field('mail',"user_$curauth->ID"); ?></a></p> 
-                <p class="subTitulo"><strong>Edad: </strong><?php the_field('edad',"user_$curauth->ID"); ?></a></p>
-                <p class="subTitulo"><strong>Estado Civil: </strong><?php the_field('estado_civil',"user_$curauth->ID"); ?></a></p>
-                <p class="subTitulo"><strong>Grupos y asociaciones:  <br /></strong><?php the_field('grupos_y_asociaciones',"user_$curauth->ID"); ?></a></p>
-                <p class="subTitulo"><strong>Actividades y talentos: <br /></strong><?php the_field('actividades_y_talentos',"user_$curauth->ID"); ?></a></p>
-                <p class="subTitulo"><strong>Grupo Familiar: </strong><?php the_field('grupo_familiar',"user_$curauth->ID"); ?></a></p>
+                <?php if(get_field('empresa',"user_$curauth->ID")){?><p class="subTitulo"><strong>Empresa:</strong> <?php the_field('empresa',"user_$curauth->ID"); ?></p><?php } ?>
+                <?php if(get_field('cargo',"user_$curauth->ID")){?><p class="subTitulo"><strong>Cargo:</strong> <?php the_field('cargo',"user_$curauth->ID"); ?></p><?php } ?>
+                <?php if(get_field('telefono',"user_$curauth->ID")){?><p class="subTitulo"><strong>Teléfono:</strong> <?php the_field('telefono',"user_$curauth->ID"); ?></p> <?php } ?>               
+                <?php if(get_field('mail',"user_$curauth->ID")){?><p class="subTitulo"><strong>Mail: </strong> <a href="mailto:<?php the_field('mail',"user_$curauth->ID");?>"><?php the_field('mail',"user_$curauth->ID"); ?></a></p> <?php } ?>
+                <?php if(get_field('edad',"user_$curauth->ID")){?><p class="subTitulo"><strong>Edad: </strong><?php the_field('edad',"user_$curauth->ID"); ?></a></p><?php } ?>
+                <?php if(get_field('estado_civil',"user_$curauth->ID")){?>
+                    <p class="subTitulo"><strong>Estado Civil: </strong><?php the_field('estado_civil',"user_$curauth->ID"); ?></a></p>
+                <?php } ?>
+                <?php if(get_field('grupos_y_asociaciones',"user_$curauth->ID")){?>
+                    <p class="subTitulo"><strong>Grupos y asociaciones:  <br /></strong>
+                <?php get_field('grupos_y_asociaciones',"user_$curauth->ID"); ?></a></p><?php } ?>
+                <?php if(get_field('actividades_y_talentos',"user_$curauth->ID")){?>
+                    <p class="subTitulo"><strong>Actividades y talentos: <br /></strong><?php the_field('actividades_y_talentos',"user_$curauth->ID"); ?></a></p>
+                <?php } ?>
+                
+                <?php if(get_field('grupo_familiar',"user_$curauth->ID")){?>
+                    <p class="subTitulo"><strong>Grupo Familiar: </strong><?php the_field('grupo_familiar',"user_$curauth->ID"); ?></a></p>
+                <?php } ?>
 
             </div>
             <?php
