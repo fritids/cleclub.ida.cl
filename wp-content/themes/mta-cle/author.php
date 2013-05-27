dc4037ce50075ba4348c7ba7bb09168c0b4328a3
Jorge Codina <jorge@ida.cl>
2013-05-27 18:04:46 -0500

    author breadcrumb

--- wp-content/themes/mta-cle/author.php
+++ wp-content/themes/mta-cle/author.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 <div id="contenido">
     <div id="leftSide" class="authorBio clearfix pad column8 down downV">
+        <?php echo breadcrumb(); ?> 
         <div  class="authorWrap">
         <?php
         if (is_user_logged_in()) {
