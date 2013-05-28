aa74733d0e5ae4fd628f18de90106a39d9335008
francisco aldana <aldana.fco@gmail.com>
2013-05-27 22:52:20 -0400

    asdasd

--- wp-content/themes/mta-cle/financiero-news.php
+++ wp-content/themes/mta-cle/financiero-news.php
@@ -12,6 +12,9 @@
 <meta name="MobileOptimized" content="320">
 <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
 <link rel="stylesheet" href="http://cleclub.ida.cl/wp-content/themes/mta-cle/style.css" type="text/css"/>
+<script src="<?php bloginfo('template_directory'); ?>/js/modernizr.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/_js/jquery-1.7.1.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/script.js"></script>
 <title></title>
 <style type="text/css" media="screen">
 * {
